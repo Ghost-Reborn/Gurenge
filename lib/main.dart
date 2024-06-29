885e471d71415c3697053f38f050fe1a9fcaed73
Ghost-Reborn <abhinandammanamkandy@gmail.com>
2024-06-29 20:55:07 +0530

    Searchable Anime

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,16 @@ class _TestWidgetState extends State<TestWidget> {
   String testText = "Loading...";
   List<AllAnime> allAnimes = [];
 
+  final TextEditingController controller = TextEditingController();
+
+  void performSearch(String query) async {
+    AllAnimeParser allAnimeParser = AllAnimeParser();
+    List<AllAnime> allAnime = await allAnimeParser.parseSearchAnime(query);
+    setState(() {
+      allAnimes = allAnime;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -45,7 +55,8 @@ class _TestWidgetState extends State<TestWidget> {
 
   void fetchData() async {
     AllAnimeParser allAnimeParser = AllAnimeParser();
-    List<AllAnime> allAnime = await allAnimeParser.parseSearchAnime("One Piece");
+    List<AllAnime> allAnime =
+        await allAnimeParser.parseSearchAnime("One Piece");
     setState(() {
       allAnimes = allAnime;
     });
@@ -53,21 +64,49 @@ class _TestWidgetState extends State<TestWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.redAccent,
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, // Number of widgets in a row
-          crossAxisSpacing: 2.0,
-          mainAxisSpacing: 2.0,
-          childAspectRatio: 2/3, // Adjust as necessary
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: 'Search',
+              border: OutlineInputBorder(),
+              suffixIcon: IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  performSearch(controller.text);
+                },
+              ),
+            ),
+            onSubmitted: (query) {
+              performSearch(query);
+            },
+          ),
         ),
-        itemCount: allAnimes.length,
-        itemBuilder: (context, index) {
-          return AnimeThumbnail(
-              allAnimes[index].name, allAnimes[index].thumbnail, () {});
-        },
-      ),
+        Expanded(
+          child: Container(
+            color: Colors.redAccent,
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // Number of widgets in a row
+                crossAxisSpacing: 2.0,
+                mainAxisSpacing: 2.0,
+                childAspectRatio: 2 / 3, // Adjust as necessary
+              ),
+              itemCount: allAnimes.length,
+              itemBuilder: (context, index) {
+                return AnimeThumbnail(
+                  allAnimes[index].name,
+                  allAnimes[index].thumbnail,
+                  () {},
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
